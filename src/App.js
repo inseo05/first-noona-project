572cd523f038a18a26c8ecb4781caed6bc4b45a3
Inseo Sung <sungis05@naver.com>
2024-11-13 02:16:48 +0900

    #7 state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
 function App() {
-  return
-  <div className="App">;</div>
-
+  return (
+  <div className="App">
+    <Hello />
+    <Hello />
+    <Hello />
+  </div>
+  );
 }
 
 export default App;
---
+++ src/component/Hello.js
@@ -0,0 +1,15 @@
+import {useState} from "react";
+
+export default function Hello(){
+    //let name="Mike";
+    const[name,setName]=useState('Mike');
+
+
+    return (<div>
+        <h1>state</h1>
+        <h2 id="name">{name}</h2>
+        <button onClick={()=>{
+            setName(name==="Mike"?"Jane":"Mike")}}>Change</button>
+        </div>
+    );
+}
